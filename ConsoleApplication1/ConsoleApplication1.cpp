53cf5bc040b97c86b1216d51b54a73f4808293ba
ichool <faisalaja2653@gmail.com>
2024-05-13 10:32:51 +0700

    membuat class

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,5 +2,20 @@
 #include <string>
 using namespace std;
 
+class mahasiswa
+{
+public:
+	static int nim;
+	int id;
+	string nama;
 
+	void setID();
+	void printAll();
+	mahasiswa(string pnama)
+	{
+		nama = pnama;
+		setID();
+	}
+};
+int mahasiswa::nim = 10;
 
